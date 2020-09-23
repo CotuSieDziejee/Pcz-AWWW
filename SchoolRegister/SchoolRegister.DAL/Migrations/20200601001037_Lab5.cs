e8e5d83352b3ed4cad8b8dc415b0271c66a43564
CotuSieDziejee <kam.gro@o2.pl>
2020-09-23 20:21:19 +0200

    SchoolRegister

---
+++ SchoolRegister/SchoolRegister.DAL/Migrations/20200601001037_Lab5.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolRegister.DAL.Migrations
+{
+    public partial class Lab5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoleValue",
+                table: "AspNetRoles",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoleValue",
+                table: "AspNetRoles");
+        }
+    }
+}
---
+++ SchoolRegister/SchoolRegister.Services/Migrations/20200601000135_Lab3.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolRegister.DAL.Migrations
+{
+    public partial class Lab3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Groups_GroupId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subjects_AspNetUsers_TeacherId",
+                table: "Subjects");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Grade",
+                table: "Grade");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeacherId",
+                table: "Subjects",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StudentId",
+                table: "Grade",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Grade",
+                table: "Grade",
+                columns: new[] { "DateOfIssue", "StudentId", "SubjectId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Groups_GroupId",
+                table: "AspNetUsers",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subjects_AspNetUsers_TeacherId",
+                table: "Subjects",
+                column: "TeacherId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Groups_GroupId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subjects_AspNetUsers_TeacherId",
+                table: "Subjects");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Grade",
+                table: "Grade");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeacherId",
+                table: "Subjects",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StudentId",
+                table: "Grade",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Grade",
+                table: "Grade",
+                column: "DateOfIssue");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Groups_GroupId",
+                table: "AspNetUsers",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subjects_AspNetUsers_TeacherId",
+                table: "Subjects",
+                column: "TeacherId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
